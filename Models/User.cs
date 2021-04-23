0edb9692b141f874ee2e6919ee035fb0e0693aea
arty-souls <swtshreya1999@gmail.com>
2021-04-23 14:54:37 +0530

    Revert "Revert "So many changes.""
    This reverts commit 14e8b80f6bb51be722b20f63d8675e9ec71e8261.

---
+++ Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ArtGallery.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your Full Name..")]
+        [Display(Name = "Full Name")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Email...")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Message...")]
+        [Display(Name = "Message")]
+        public string Message { get; set; }
+
+    }
+}
